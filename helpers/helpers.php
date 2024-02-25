009f9c264b3c42a3d0de0b6aa1a316c476a6aa72
nguyenanhung <info@nguyenanhung.com>
2024-02-25 13:51:46 +0700

    Add parseExpressionOptions

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -1,1 +1,22 @@
 <?php
+if (!function_exists('parseExpressionOptions')) {
+	function parseExpressionOptions($serviceId = 'test', $options = array(), $default = ' [name]'): string
+	{
+		$expression = $serviceId;
+		if (!empty($default)) {
+			$expression .= " [name]";
+		}
+		if (!empty($options)) {
+			foreach ($options as $option) {
+				$explodeOption = explode('|', $option);
+				$countExplodeOption = count($explodeOption);
+				if ($countExplodeOption > 1) {
+					$expression .= " [-" . $explodeOption[0] . "|--" . $explodeOption[1] . "]";
+				} else {
+					$expression .= " [--" . $option . "]";
+				}
+			}
+		}
+		return trim($expression);
+	}
+}
